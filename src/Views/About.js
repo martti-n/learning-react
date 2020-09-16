e8dbaa66b483696067a63934a0ab519868e5c268
Martti <martti@scr.ee>
2020-09-16 23:42:57 +0300

    first steps into React

---
+++ src/Views/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">This is the about page</h1>
+    </div>
+  );
+}
+
+export default About;
